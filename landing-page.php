6eb5a22b50941ac0224973a8aee0c500a39becf7
CheechRockwizard <136230172+LamcatUK@users.noreply.github.com>
2024-01-17 18:44:03 +0000

    Create landing-page.php

---
+++ landing-page.php
@@ -0,0 +1,21 @@
+<?php
+/* Template Name: Landing Page */
+
+// Exit if accessed directly.
+defined('ABSPATH') || exit;
+
+get_header();
+?>
+<main>
+    <div class="container-xl">
+        <a href="https://iology.co.uk/book-appointment/" class="btn btn-primary">Book an Appointment</a>
+
+        <a href="https://api.whatsapp.com/send?phone=+447496971203&text=Hi,%20I%27m%20contacting%20you%20from%20the%20iology%20website."
+            class="btn btn-primary">Contact via WhatsApp</a>
+
+        <a href="https://iology.co.uk/leave-review/" class="btn btn-primary">Leave a Review</a>
+
+    </div>
+</main>
+<?php
+get_footer();
